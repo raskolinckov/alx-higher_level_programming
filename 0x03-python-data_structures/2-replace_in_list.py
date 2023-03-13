96271a760f43f23db48215c1f126610f1aacf962
raskolinckov <103078933+raskolinckov@users.noreply.github.com>
2023-03-13 18:08:56 +0200

    Update 2-replace_in_list.py

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-def replace_in_list(my_list, idx, element):
-    if idx < len(my_list) and idx >= 0:
-        my_list[idx] = element
-    return (my_list)
+def element_at(my_list, idx):
+    """Retrive an element from a list."""
+    if idx < 0 or idx > (len(my_list) - 1):
+        return None
+    return (my_list[idx])
